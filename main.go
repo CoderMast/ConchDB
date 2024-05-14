39f5146136fe6f9381570919a75b50c1a42ecc45
codermast <codermast@qq.com>
2024-05-14 10:26:03 +0800

    Conch DBM

--- main.go
+++ main.go
@@ -4,20 +4,35 @@ import (
 	"ConchDB/backend/services"
 	"embed"
 	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/menu"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/linux"
+	"github.com/wailsapp/wails/v2/pkg/options/mac"
+	"runtime"
 )
 
 //go:embed all:frontend/dist
 var assets embed.FS
 
+//go:embed build/appicon.png
+var icon []byte
+
 func main() {
 	// Create an instance of the app structure
 	connection := services.NewConnection()
 
+	// Create Menu for Mac OS
+	appMenu := menu.NewMenu()
+	if runtime.GOOS == "darwin" {
+		appMenu.Append(menu.AppMenu())
+		appMenu.Append(menu.EditMenu())
+		appMenu.Append(menu.WindowMenu())
+	}
+
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "ConchDB",
+		Title:  "Conch DB Management",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
@@ -27,6 +42,24 @@ func main() {
 		OnStartup:        connection.Startup,
 		Bind: []interface{}{
 			connection,
+		},
+		// 配置 Mac 下的桌面选项
+		Mac: &mac.Options{
+			TitleBar: mac.TitleBarDefault(),
+			About: &mac.AboutInfo{
+				Title:   "Conch DBM Management",
+				Message: "A modern lightweight cross-platform Database desktop client.\n 一个现代化轻量级跨平台的数据库桌面客户端。\n\nCopyright © 2024 \n Author By CoderMast",
+				Icon:    icon,
+			},
+			WebviewIsTransparent: false,
+			WindowIsTranslucent:  false,
+		},
+		// 配置 Linux 下的桌面选项
+		Linux: &linux.Options{
+			ProgramName:         "Conch DB Management",
+			Icon:                icon,
+			WebviewGpuPolicy:    linux.WebviewGpuPolicyOnDemand,
+			WindowIsTranslucent: true,
 		},
 	})
 
