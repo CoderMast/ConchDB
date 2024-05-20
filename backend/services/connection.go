0d75f73b1d5662e032889aacb7a30796c41f5d1b
codermast <codermast@qq.com>
2024-05-20 23:12:41 +0800

    重构UI

--- backend/services/connection.go
+++ backend/services/connection.go
@@ -3,8 +3,8 @@ package services
 import (
 	"context"
 	"database/sql"
-	"encoding/json"
 	"fmt"
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -51,68 +51,6 @@ func (c *Connection) Connection(dbtype string, host string, port string, usernam
 
 // Execute 执行SQL
 func (c *Connection) Execute(sql string) string {
-	if dbConnection == nil {
-		return fmt.Sprintf("Error:请先连接数据库!")
-	}
-
-	rows, err := dbConnection.Query(sql)
-
-	if err != nil {
-		return fmt.Sprintf("Error:%s!", err)
-	}
-	// 确保关闭
-	defer rows.Close()
-
-	// 创建一个 Map 数组，用来存储返回数据
-	results := make([]map[string]string, 0)
-
-	// 获取列名
-	columns, err := rows.Columns()
-
-	if err != nil {
-		return fmt.Sprintf("Error:%s!", err)
-	}
-
-	// 将列名加进返回切片对象
-	columnMap := make(map[string]string)
-	for _, column := range columns {
-		columnMap[column] = column
-	}
-	results = append(results, columnMap)
-
-	// 将内容添加进返回切片对象
-	for rows.Next() {
-		// 创建一个与列数量相同切片
-		values := make([]string, len(columns))
-
-		// 对应的指针切片
-		ptrs := make([]interface{}, len(columns))
-
-		for i := range values {
-			ptrs[i] = &values[i]
-		}
-		// 这里实际上传入的事 value 中前 len(columns) 列的地址，故从 value 中能取到最后的实际数据
-		err := rows.Scan(ptrs...)
-
-		if err != nil {
-			return fmt.Sprintf("Error:%s!", err)
-		}
-
-		// 创建一个值的 map 切片
-		valuesMap := make(map[string]string)
-
-		for i, value := range values {
-			valuesMap[columns[i]] = value
-		}
-
-		results = append(results, valuesMap)
-	}
-
-	// 将返回对象转换为 JSON
-	jsonResults, err := json.Marshal(results)
-
-	if err != nil {
-		return fmt.Sprintf("Error:%s!", err)
-	}
-	return fmt.Sprintf("%s", jsonResults)
+	dbConnection.Query("123")
+	return "项目重构ing..."
 }
--- frontend/src/components/Execute.vue
+++ frontend/src/components/Execute.vue
@@ -1,71 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { Execute } from "../../wailsjs/go/services/Connection";
 
-import { useMessage } from 'naive-ui';
-
-const message = useMessage();
-
-// 执行 SQL
-const sql = ref("select * from users;");
-const sqlResult = ref();
-const sqlResultHead = ref()
-const sqlResultBody = ref()
-
-
-function execute() {
-  // 只要执行，就记录日志
-  addSqlExecuteLog();
-  // 实际请求
-  Execute(sql.value).then(result => {
-    if (result.substring(0, 5) != "Error") {
-      console.log(result)
-      // 将 JSON 字符串转换为二维数组
-      const jsonResult = JSON.parse(result);
-      console.log(jsonResult)
-      // 将二维数组赋值给页面变量
-      sqlResult.value = jsonResult;
-      sqlResultHead.value = jsonResult[0];
-      sqlResultBody.value = jsonResult.slice(1);
-
-
-      // 执行成功提示
-      message.success("执行成功！");
-    } else {
-      // 执行失败的提示信息
-      message.error(result)
-      console.log(result)
-    }
-  })
-}
-
-// SQL 语句执行记录
-let executeList = ref(new Array<string>());
-
-// 增加 SQL 执行记录
-function addSqlExecuteLog() {
-  executeList.value.push(sql.value)
-}
 </script>
 
 <template>
 
-  <n-row style="width: 80vw;margin: 0 auto;">
-    <n-col :span="8" style="margin: auto">请输入你要执行的SQL语句：</n-col>
-    <n-col :span="16" style="display: flex;margin: auto">
-      <n-input type="text" v-model="sql" />
-      <n-button type="primary" @click="execute">执行SQL</n-button>
-    </n-col>
-  </n-row>
-
-
-  <!-- 表单渲染 -->
-  <!-- <n-data-table :columns="sqlResultBody[0]" :data="sqlResultBody" style="width: 100%" /> -->
-
-  <!-- 日志渲染 -->
-  <!-- <n-card style="width: 100vw">
-    <p style="text-align: center;" v-for="(item, index) in executeList" :key="index" class="text item">{{ item }}</p>
-  </n-card> -->
 </template>
 
 <style scoped></style>
